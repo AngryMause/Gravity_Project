15a204f7dc77479867004ad32a03e4691d8ff8ff
AngryMause <chostblade@gmail.com>
2022-09-25 00:05:18 +0300

    all done

--- app/src/main/java/com/example/gravity_project/ui/fragment/loadingscreen/LoadingFragment.kt
+++ app/src/main/java/com/example/gravity_project/ui/fragment/loadingscreen/LoadingFragment.kt
@@ -2,12 +2,16 @@ package com.example.gravity_project.ui.fragment.loadingscreen
 
 import android.os.Bundle
 import android.view.View
+import androidx.core.view.isVisible
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.asFlow
 import androidx.lifecycle.lifecycleScope
+import com.example.gravity_project.R
 import com.example.gravity_project.databinding.FragmentLoadingBinding
 import com.example.gravity_project.ui.fragment.BaseFragment
 import com.example.gravity_project.ui.fragment.webviewscreen.WebViewFragment
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
@@ -23,8 +27,15 @@ class LoadingFragment : BaseFragment<FragmentLoadingBinding>(FragmentLoadingBind
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         viewLifecycleOwner.lifecycleScope.launch {
-            loadingScreenViewModel.response.observe(viewLifecycleOwner) {
-                replaceFragment(WebViewFragment.newInstance(it))
+            loadingScreenViewModel.response.asFlow().collect {
+                if (it.isEmpty()) {
+                    binding.myProgressBar.isVisible = true
+                } else {
+                    binding.myProgressBar.isVisible = false
+                    binding.loading.setText(R.string.success)
+                    delay(1500)
+                    replaceFragment(WebViewFragment.newInstance(it))
+                }
             }
         }
     }
